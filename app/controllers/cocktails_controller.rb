5f25f33eef3c74d35775d14bc9eecccf7d01ec79
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-26 10:43:27 +0200

    bug corrected

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,6 @@
 class CocktailsController < ApplicationController
 
-  before_action :find_cocktail, only: [:show]
+  before_action :find_cocktail, only: [:show, :destroy]
 
   def index
     @cocktails = Cocktail.all
@@ -23,6 +23,11 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
 
   private
 
@@ -31,7 +36,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo, :photo_cache)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new,:create, :show] do
+  resources :cocktails, only: [:index, :new,:create, :show, :destroy] do
     resources :doses, only: [:create]
   end
   resources :doses, only: :destroy
